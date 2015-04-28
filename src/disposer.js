3aca85ef1b12536dcaee1f7f221f81825837cb8f
malikov <malikovincent@gmail.com>
2015-04-28 15:47:14 -0400

    extracting knockout-disposer

---
+++ src/disposer.js
@@ -0,0 +1,27 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout'],
+    function(ko) {
+        'use strict';
+
+        function KnockoutDisposer() {
+            this.subscriptions = [];
+        }
+
+        KnockoutDisposer.prototype.add = function(subscription) {
+            this.subscriptions.push(subscription);
+        };
+
+        KnockoutDisposer.prototype.dispose = function() {
+            for (var i in this.subscriptions) {
+                var subscription = this.subscriptions[i];
+
+                if(subscription && subscription.dispose){
+                    subscription.dispose();
+                }
+            }
+        };
+
+        return KnockoutDisposer;
+    });

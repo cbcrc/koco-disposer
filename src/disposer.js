6da90cdc64b23529e66e4e834d7d64f9a353a801
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:20:54 -0400

    update to koco2

--- src/disposer.js
+++ src/disposer.js
@@ -2,27 +2,25 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 
-define(['knockout'],
-    function(ko) {
-        'use strict';
+import ko from 'knockout';
 
-        function KnockoutDisposer() {
-            this.subscriptions = [];
+
+function KnockoutDisposer() {
+    this.subscriptions = [];
+}
+
+KnockoutDisposer.prototype.add = function(subscription) {
+    this.subscriptions.push(subscription);
+};
+
+KnockoutDisposer.prototype.dispose = function() {
+    for (var i in this.subscriptions) {
+        var subscription = this.subscriptions[i];
+
+        if (subscription && subscription.dispose) {
+            subscription.dispose();
         }
+    }
+};
 
-        KnockoutDisposer.prototype.add = function(subscription) {
-            this.subscriptions.push(subscription);
-        };
-
-        KnockoutDisposer.prototype.dispose = function() {
-            for (var i in this.subscriptions) {
-                var subscription = this.subscriptions[i];
-
-                if(subscription && subscription.dispose){
-                    subscription.dispose();
-                }
-            }
-        };
-
-        return KnockoutDisposer;
-    });
+export default KnockoutDisposer;
